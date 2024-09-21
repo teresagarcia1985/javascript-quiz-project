d8624e1f30458b708c34baf0a6e119db213da985
Teresa Garcia <teresagarcia@Teresas-MacBook-Air.local>
2024-09-21 16:31:44 +0200

    ‘testing’

--- tests/question.spec.js
+++ tests/question.spec.js
@@ -11,12 +11,12 @@ describe("Question", () => {
     });
 
     it("should receive 'text' string as its 1st argument and assign it to 'text' property", () => {
-    // String with the question text to be used in the test and passed to the Quiz constructor
+      // String with the question text to be used in the test and passed to the Quiz constructor
       const testText = "What is 2 + 2?";
 
       // Instantiate a new Question object with the test text string
       const question = new Question(testText, [], "test");
-      
+
       // Check if the 'text' property of the Question object is equal to the test text string
       expect(question.text).toEqual(testText);
     });
@@ -27,7 +27,7 @@ describe("Question", () => {
 
       // Instantiate a new Question object with the test choices array
       const question = new Question("test", testChoices, "test");
-      
+
       // Check if the 'choices' property of the Question object is equal to the test choices array
       expect(question.choices).toEqual(testChoices);
     });
@@ -35,29 +35,28 @@ describe("Question", () => {
     it("should receive 'answer' string as its 3rd argument and assign it to 'answer' property", () => {
       // String with the question answer to be used in the test and passed to the Quiz constructor
       const testAnswer = "This is a test answer";
-      
+
       // Instantiate a new Question object with the test answer string
       const question = new Question("test", [], testAnswer);
-      
+
       // Check if the 'answer' property of the Question object is equal to the test answer string
       expect(question.answer).toEqual(testAnswer);
     });
 
-    it("should receive 'difficulty' string as its 4th argument and assign it to 'difficulty' property", () => {      
+    it("should receive 'difficulty' string as its 4th argument and assign it to 'difficulty' property", () => {
       // Instantiate a new Question object and pass the test difficulty (number)
       const question = new Question("question", [], "test", 2);
-      
+
       // Check if the 'difficulty' property of the Question object is equal to the test difficulty (number)
       expect(question.difficulty).toEqual(2);
     });
-
   });
 
   describe("shuffleChoices() method", () => {
     it("should be defined", () => {
       // Instantiate a new Question object
       const question = new Question("test", [], "test");
-      
+
       // Check if the 'shuffleChoices' method is defined
       expect(question.shuffleChoices).toBeDefined();
     });
@@ -65,7 +64,7 @@ describe("Question", () => {
     it("should be a function", () => {
       // Instantiate a new Question object
       const question = new Question("test", [], "test");
-      
+
       // Check if the .shuffleChoices is a function
       expect(typeof question.shuffleChoices).toBe("function");
     });
@@ -73,22 +72,38 @@ describe("Question", () => {
     it("should receive no arguments", () => {
       // Instantiate a new Question object
       const question = new Question("test", [], "test");
-      
+
       // Check that the .shuffleChoices method takes no arguments
       expect(question.shuffleChoices.length).toEqual(0);
     });
 
     it("should shuffle the items in the 'choices' array", () => {
       // Array with the question choices to be used in the test and passed to the Quiz constructor
-      const testChoices = ["choice1", "choice2", "choice3", "choice4", "choice5", "choice6", "choice7"];
-      
+      const testChoices = [
+        "choice1",
+        "choice2",
+        "choice3",
+        "choice4",
+        "choice5",
+        "choice6",
+        "choice7",
+      ];
+
       // Instantiate a new Question object with the test choices array
       const question = new Question("test", testChoices, "test");
       // Call the shuffleChoices() method on the Question object
       question.shuffleChoices();
-      
+
       // Check that the 'choices' property of the Question object is not equal to the test choices array (i.e. it has been changed/shuffled)
-      expect(question.choices).not.toEqual(["choice1", "choice2", "choice3", "choice4", "choice5", "choice6", "choice7"]);
+      expect(question.choices).not.toEqual([
+        "choice1",
+        "choice2",
+        "choice3",
+        "choice4",
+        "choice5",
+        "choice6",
+        "choice7",
+      ]);
       expect(question.choices.length).toEqual(testChoices.length);
     });
   });
