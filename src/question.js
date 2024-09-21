e7866356336ef000d43636e6b161aba93b8b2943
Mirela Dragulescu <dragulescumirela@gmail.com>
2024-09-21 17:13:14 +0200

    Day 3

--- src/question.js
+++ src/question.js
@@ -1,7 +1,23 @@
 class Question {
   // YOUR CODE HERE:
-  asasasasa;
-  // 1. constructor (text, choices, answer, difficulty)
 
+  // 1. constructor (text, choices, answer, difficulty)
+  constructor(text, choices, answer, difficulty) {
+    this.text = text;
+    this.choices = choices;
+    this.answer = answer;
+    this.difficulty = difficulty;
+  }
   // 2. shuffleChoices()
+  shuffleChoices() {
+    for (let i = this.choices.length - 1; i > 0; i--) {
+      //return Math.floor(Math.random() * max);
+      const shuffle = Math.floor(Math.random() * (i + 1));
+      [this.choices[i], this.choices[shuffle]] = [
+        this.choices[shuffle],
+        this.choices[i],
+      ];
+    }
+    return this.choices;
+  }
 }
--- src/quiz.js
+++ src/quiz.js
@@ -1,15 +1,31 @@
 class Quiz {
-    // YOUR CODE HERE:
-    //
-    // 1. constructor (questions, timeLimit, timeRemaining)
+  // YOUR CODE HERE:
 
-    // 2. getQuestion()
-    
-    // 3. moveToNextQuestion()
+  constructor(questions, timeLimit, timeRemaining) {
+    this.questions = questions;
+    this.timeLimit = timeLimit;
+    this.timeRemaining = timeRemaining;
+    this.correctAnswers = 0;
+    this.currentQuestionIndex = 0;
+  }
 
-    // 4. shuffleQuestions()
+  getQuestion() {
+    return this.questions[this.currentQuestionIndex];
+  }
 
-    // 5. checkAnswer(answer)
+  moveToNextQuestion() {
+    for (
+      let currentQuestionIndex = 0;
+      currentQuestionIndex < this.questions.length;
+      currentQuestionIndex++
+    ) {
+      return (currentQuestionIndex += currentQuestionIndex + 1);
+    }
+  }
 
-    // 6. hasEnded()
+  // 4. shuffleQuestions()
+
+  // 5. checkAnswer(answer)
+
+  // 6. hasEnded()
 }
