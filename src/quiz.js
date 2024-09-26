4a96f532b671652337f817e9c8e4f011b1e75832
Mirela Dragulescu <dragulescumirela@gmail.com>
2024-09-26 21:31:37 +0200

    Day 2

--- src/quiz.js
+++ src/quiz.js
@@ -14,18 +14,54 @@ class Quiz {
   }
 
   moveToNextQuestion() {
-    for (
-      let currentQuestionIndex = 0;
-      currentQuestionIndex < this.questions.length;
-      currentQuestionIndex++
-    ) {
-      return (currentQuestionIndex += currentQuestionIndex + 1);
-    }
+    this.currentQuestionIndex = this.currentQuestionIndex + 1;
   }
 
   // 4. shuffleQuestions()
+  shuffleQuestions() {
+    for (let i = this.questions.length - 1; i > 0; i--) {
+      //return Math.floor(Math.random() * max);
+      const shuffle = Math.floor(Math.random() * (i + 1));
+      [this.questions[i], this.questions[shuffle]] = [
+        this.questions[shuffle],
+        this.questions[i],
+      ];
+    }
+    return this.choices;
+  }
 
   // 5. checkAnswer(answer)
 
-  // 6. hasEnded()
+  checkAnswer(answer) {
+    let currentQuestion = this.questions[this.currentQuestionIndex];
+    if (currentQuestion.answer === answer) {
+      this.correctAnswers++;
+    }
+  }
+
+  hasEnded() {
+    if (this.currentQuestionIndex === this.questions.length) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+  filterQuestionsByDifficulty(difficulty) {
+    if (difficulty >= 1 && difficulty <= 3) {
+      const filteredQuestions = this.questions.filter((question) => {
+        if (question.difficulty === difficulty) {
+          return true;
+        }
+      });
+      this.questions = filteredQuestions;
+    }
+  }
+  averageDifficulty() {
+    const sum = this.questions.reduce((acc, currQuestion) => {
+      return acc + currQuestion.difficulty;
+    }, 0);
+    return sum / this.questions.length;
+  }
 }
+
+// 6. hasEnded()
